2020f0fe82980cdfd12245e68ffe4f2ba6af757a
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-03-15 01:45:27 +0700

    Fix redirect

--- src/app/(onkey)/afterdark/route.ts
+++ src/app/(onkey)/afterdark/route.ts
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const cookie = await cookies();
   cookie.set("theme", "afterdark");
-  return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL));
+  redirect("/");
 }
