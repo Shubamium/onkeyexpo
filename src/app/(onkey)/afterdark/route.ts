13720b329f654e9e7c223643b8a00785fd302668
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-03-15 01:52:17 +0700

    Finalized switcher

--- src/app/(onkey)/afterdark/route.ts
+++ src/app/(onkey)/afterdark/route.ts
@@ -5,5 +5,5 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const cookie = await cookies();
   cookie.set("theme", "afterdark");
-  redirect("/");
+  return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL));
 }
