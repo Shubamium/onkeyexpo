418593e340ea09419a071d8abdb10c3e1b55cfe9
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-03-17 00:07:39 +0700

    Add modal

--- src/app/(onkey)/components/adSwitch/AfterDarkSwitch.tsx
+++ src/app/(onkey)/components/adSwitch/AfterDarkSwitch.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import { TbRating18Plus } from "react-icons/tb";
 
 type Props = {
   initTheme: string;
@@ -26,25 +28,70 @@ export default function AfterDarkSwitch({ initTheme }: Props) {
     //   router.push(route);
     // }, 100);
   };
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const [ageModal, setAgeModal] = useState(false);
+  const setDarkTheme = () => {
+    setAgeModal(false);
+    switchTheme("/afterdark");
+    setTimeout(() => {
+      setActiveTheme("afterdark");
+    }, 300);
+  };
   return (
-    <button
-      id="switch"
-      className={`${activeTheme === "afterdark" ? "active" : ""}`}
-      onClick={() => {
-        if (activeTheme === "afterdark") {
-          setActiveTheme("onkey");
-          switchTheme("/onkey");
-        } else {
-          setActiveTheme("afterdark");
-          switchTheme("/afterdark");
-        }
-        // setBtn(!btn);
-      }}
-    >
-      {" "}
-      <span>AFTER DARK</span>
-      <div className="switcher"></div>
-    </button>
+    <>
+      <button
+        id="switch"
+        className={`${activeTheme === "afterdark" ? "active" : ""}`}
+        onClick={() => {
+          if (activeTheme === "afterdark") {
+            setActiveTheme("onkey");
+            switchTheme("/onkey");
+          } else {
+            setAgeModal(true);
+          }
+          // setBtn(!btn);
+        }}
+      >
+        {" "}
+        <span>AFTER DARK {ageModal}</span>
+        <div className="switcher"></div>
+      </button>
+      {mounted &&
+        createPortal(
+          <div className={` age-modal ${ageModal ? "act" : "closed"}`}>
+            <div className="panel">
+              <div className="textpart">
+                <TbRating18Plus />
+                <h2>Age Verification</h2>
+                <p>
+                  OnKey After Dark may contain content intended for adults. By
+                  clicking confirm, you confirm that you are 18 years of age or
+                  older (or the legal age of majority in your location).
+                </p>
+              </div>
+
+              <div className="action">
+                <button className="btn btn-main con" onClick={setDarkTheme}>
+                  CONFIRM
+                </button>
+                <button
+                  className="btn btn-main can"
+                  onClick={() => {
+                    setAgeModal(false);
+                  }}
+                >
+                  CANCEL
+                </button>
+              </div>
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
   );
 }
